1efb7b44d60b08ce43d30b539662b78d19077d38
Moaz Elbahr <moazelbahr@gmail.com>
2025-07-17 09:16:21 +0300

    Correcting BOM field domain

--- models/sale_order_line_inherit.py
+++ models/sale_order_line_inherit.py
@@ -4,17 +4,24 @@ class SaleOrderLineBOM(models.Model):
     _inherit = "sale.order.line"
 
 
-    bom_id = fields.Many2one('mrp.bom', string="BOM",
-    domain="[('product_tmpl_id', '=', product_template_id), ('product_tmpl_id.is_mto', '=', True),"
-           "('product_tmpl_id.is_manufacture', '=', True)]")
+    bom_id = fields.Many2one('mrp.bom', string="BOM")
+    is_mto_and_manufacture = fields.Boolean(compute="_compute_is_mto_and_manufacture", store=True)
 
-    @api.onchange('product_id', 'product_template_id')
-    def _onchange_product_id(self):
-        for rec in self:
-            if rec.product_template_id:
-                bom = self.env['mrp.bom'].search([
-                    ('product_tmpl_id', '=', rec.product_template_id.id),
-                    ('product_tmpl_id.is_mto', '=', True),
-                    ('product_tmpl_id.is_manufacture', '=', True),
-                ], limit=1)
-                rec.bom_id = bom
+    @api.depends('product_template_id', 'product_template_id.route_ids')
+    def _compute_is_mto_and_manufacture(self):
+        for line in self:
+            line.is_mto_and_manufacture = False
+            product = line.product_template_id
+            if line.is_mto:
+                for route in product.route_ids:
+                    for rule in route.rule_ids:
+                        if rule.action == 'manufacture':
+                            line.is_mto_and_manufacture = True
+
+    @api.onchange('is_mto_and_manufacture')
+    def _onchange_is_mto_and_manufacture(self):
+        for line in self:
+            bom = self.env['mrp.bom'].search([
+                ('product_tmpl_id', '=', line.product_template_id.id)
+            ], limit=1)
+            line.bom_id = bom
